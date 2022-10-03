ec4663ab148e56438285e0d4f177680e22712093
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-10-03 19:38:37 +0300

    Update lab12.py

--- lab12.py
+++ lab12.py
@@ -7,3 +7,23 @@ try:
     print('\n'.join(lines))
 except FileNotFoundError:
     print('File not found')
+
+    
+#частота букав 
+
+
+wholeFile = open("words.txt", 'r+').read()
+lowlet = wholeFile.lower()
+letters= list(lowlet)
+alpha = list('abcdefghijklmnopqrstuvwxyz')
+n = len(letters)
+f = float(n)
+occurrences = {}
+d = {}
+
+
+    #number of letters
+for x in alpha:
+    occurrences[x] = letters.count(x)
+    for x in occurrences:
+        print (x, occurrences[x])
