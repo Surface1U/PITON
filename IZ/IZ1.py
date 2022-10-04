ff8b379fdfbaa18bc712e9035007a48bae879817
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-10-04 10:19:36 +0300

    Update IZ1.py

--- IZ/IZ1.py
+++ IZ/IZ1.py
@@ -1,23 +1,33 @@
-firstFile = open(folder + "9a.txt", "r")
-secondFile = open(folder + "9b.txt", "r")
 
-firstLines = firstFile.readlines()
-secondLines = secondFile.readlines()
+from itertools import *
 
-firstFile.close()
-secondFile.close()
 
-readyFirstLines = list(map(str.strip, firstLines))
-readySecondLines = list(map(str.strip, secondLines))
-firstArr = []
-secondArr = []
 
-for el in readyFirstLines:
-    firstArr.append(el)
-for el in readySecondLines:
-    secondArr.append(el)
-fArr = list(map(int, firstArr))
-sArr = list(map(int, secondArr))
+#  Заводим таблицу размером n+1 строк и 100 столбцов
+#Для i-го числа a[i]:   
+  #копируете i-строку в следующую  
+  #для каждой ячейки i-й строки:
+ #    находите её сумму с a[i] (пусть t)
+#     и обновляете максимум в ячейке [t%100] следующей строки
+
+
+#s =0
+data = []
+data1 = []
+f = open("9b.txt","r")
+fa = open("9a.txt","r")
+data = [int(i) for i in f.read().split()]
+dataCop = []
+data1Cop = []
+data1 = [int(i) for i in fa.read().split()]
+
+for x in data:
+    dataCop.append(x)
+for x in data1:
+    data1Cop.append(x)
+
+fArr = list(map(int,dataCop))
+sArr = list(map(int,data1Cop))
 
 aF = [0] * 100
 bF = [0] * 100
